ef2c9189268b7ff50e71950d960749113f655e8e
Marcus Medeiros <vinicius1504_medeiros@hotmail.com>
2019-06-26 18:01:53 -0300

    creating project Bank Santander

---
+++ Bank_Santander/app/src/main/java/br/com/resourceit/nasa/bank_santander/MainActivity.kt
@@ -0,0 +1,12 @@
+package br.com.resourceit.nasa.bank_santander
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
